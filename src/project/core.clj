00913399a56821bc55671ebf620ca219231057cc
jammus <jammus@gmail.com>
2023-02-12 18:31:20 +0800

    Accept command line args

--- src/project/core.clj
+++ src/project/core.clj
@@ -1,6 +1,7 @@
 (ns project.core
-  (:require [clj-http.client :as client])
-  (:require [cheshire.core :refer [generate-string]])
+  (:require [clj-http.client :as client]
+            [clojure.tools.cli :refer [parse-opts]]
+            [cheshire.core :refer [generate-string]])
   (:gen-class))
 
 (defn fetch-json
@@ -9,9 +10,9 @@
   ([url, options]
   (:body (client/get url (into {:as :json} options)))))
 
-(defn fetch-timeline 
-  [] 
-  (fetch-json "https://mastodon.return12.net/api/v1/timelines/public"))
+(defn fetch-timeline
+  [domain]
+  (fetch-json (str "https://" domain "/api/v1/timelines/public")))
 
 (defn extract-details
   [toots]
@@ -24,8 +25,19 @@
 (def write-details-json
   (partial write-json "details.json"))
 
+(def cli-options
+  [["-d" "--domain DOMAIN" "Mastodon domain to query"]
+   ["-h" "--help"]])
+
 (defn -main
-  []
-  (->> (fetch-timeline)
-       (extract-details)
-       (write-details-json)))
+  [& args]
+  (let [{:keys [options summary]} (parse-opts args cli-options)]
+    (cond
+      (:help options)
+      (println (str "Query the supplied Mastodon instance\n" summary))
+      (nil? (:domain options))
+      (println "Missing domain argument")
+      :else
+      (->> (fetch-timeline (:domain options))
+           (extract-details)
+           (write-details-json)))))
