0372e6dfae7aa6424e76f83912dba5b333690cb9
jammus <jammus@gmail.com>
2023-02-12 18:31:03 +0800

    Refactor tests to use local let

--- test/project/core_test.clj
+++ test/project/core_test.clj
@@ -1,19 +1,13 @@
 (ns project.core-test
   (:require [clojure.test :refer :all]
-            [project.core :refer :all]))
-
-(def single-toot (
- list {:id 4 :url "https://google.com" :other "property" }
-))
-
-(def multiple-toots (
- list
-   {:id 4 :url "https://google.com" :other "property" }
-   {:id 6 :url "https://example" :ignore "this" }
-   {:id 3 :url "https://last.fm" :no "good" }
-))
+            [project.core :refer [extract-details]]))
 
 (deftest extract-details-test
+  (let [single-toot (list {:id 4 :url "https://google.com" :other "property" })
+        multiple-toots (list
+           {:id 4 :url "https://google.com" :other "property" }
+           {:id 6 :url "https://example" :ignore "this" }
+           {:id 3 :url "https://last.fm" :no "good" })]
   (testing "empty list when empty"
     (is (empty? (extract-details []))))
   (testing "strips unrequired properties"
@@ -26,4 +20,4 @@
          (list
            {:id 4 :url "https://google.com"}
            {:id 6 :url "https://example"}
-           {:id 3 :url "https://last.fm"})))))
+           {:id 3 :url "https://last.fm"}))))))
